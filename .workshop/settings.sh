3da52fa76f69a662c3c47bce83d3dbc39aa9bce0
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-11-20 16:24:54 +1100

    Tag version 1.3.

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -1,5 +1,5 @@
 WORKSHOP_NAME=lab-jupyter-notebooks-01
-WORKSHOP_IMAGE=quay.io/jupyteronopenshift/lab-jupyter-notebooks-01:master
+WORKSHOP_IMAGE=quay.io/jupyteronopenshift/lab-jupyter-notebooks-01:1.3
 RESOURCE_BUDGET=medium
 MAX_SESSION_AGE=3600
 IDLE_TIMEOUT=300
